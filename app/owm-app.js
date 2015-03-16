fac5799476a9b36fb2326352a5cbc58e52ac82ee
David Elmer <davelmer@hotmail.com>
2015-03-16 13:46:07 -0400

    Moved files

---
+++ app/owm-app.js
@@ -0,0 +1,25 @@
+(function() {
+    //"use strict";
+    
+    angular.module('OWMApp', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider){
+            $routeProvider.when('/', {
+                templateURL : 'home.html',
+                controller : 'HomeController'
+            }).when('/cities/:city', {
+                templateURL : 'city.html',
+                controller : 'CityController'
+            });
+        }])
+        .controller('HomeContoller', function($scope) {
+            //empty for now
+        })
+        .controller('CityController', function($scope, city) {
+            $scope.city = city;
+        });
+
+        //$locationProvider.html5mode({ enabled: true, requireBase: false });
+
+})();
+
+
